697161e5c482a5a30449dd7d78c2b20cde2ab1e6
Smagins <75066986+Smagins@users.noreply.github.com>
2024-06-25 01:05:35 +0300

    Silhouette.py

--- Silhouette.py
+++ Silhouette.py
@@ -31,7 +31,7 @@ if passw == "635241zQ":
             print(Fore.BLUE+"Первое правило: Если прибавить к своей просьбе причину, то отказ человека очень сильно уменьшается\nВторое правило:Хвалить человека, и обещать ему в том что он заинтересован\nГрубо говоря есть 3 пунка:\n1)Заболтать\n2)Заинтересовать\n3)Наебать\nСамое главное это то, за что можно зацепиться\nЕсть книга 'Искусство Обмана' написанная бывшим хакером Кевином Митником, где подробно рассказаны шаги и некоторые фишки социальной инженерии.\nТебе скинуть ссылку? [Да/Нет]")
             yesNo = input(Fore.RED + "#")
             if yesNo == "Да" or yesNo == "да":
-                webbrowser.open("https://royallib.com/book/mitnik_kevin/iskusstvo_obmana.html",new=2)
+                print(Fore.BLUE+"https://royallib.com/book/mitnik_kevin/iskusstvo_obmana.html")
             if yesNo == "Нет" or yesNo == "нет":
                 print(Fore.BLUE+"Надумаешь, не стесняйся, бери")
         if choose == "2":
@@ -41,13 +41,13 @@ if passw == "635241zQ":
 
             print(sas.text)
         if choose == "3":
-            webbrowser.open("https://iplogger.org/ru/", new=2)
+            print(Fore.BLUE+"https://iplogger.org/ru/")
         if choose == "4":
             run = False
         if choose == "5":
             print(Fore.RED + "Добро пожаловать на DoxBin детка!")
             time.sleep(3)
-            webbrowser.open("https://doxbin.com/", new=2)
+            print("https://doxbin.com/")
         if choose == "1488":
             print(Fore.BLUE+"Пасхалко?)")
     
